92d1ff6bf66665ad3db5bc4089e977a04411d284
zevtyardt <apklover76@gmail.com>
2024-02-28 08:42:09 +0700

    refactor(main): Refactor main.rs file
    - Reorganized the imports in the `main.rs` file for better readability and organization.
    - Updated the usage of the `clearscreen_and_show_banner` function.
    - Renamed the `init_app` function to `clearscreen_and_show_banner` for better clarity.
    - Updated the usage of the `extract_download_url` method to `extract_stream_urls` in the main loop.

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,13 @@ use std::io::{stdout, Write};
 use animeku::AnimekuCli;
 use colored::Colorize;
 use dialoguer::theme::ColorfulTheme;
-use ext::{anime::AnimeExt, movie::MovieExt, Ext};
+use ext::{
+    nontonanime::{anime::AnimeExt, movie::MovieExt},
+    Ext,
+};
 use tokio::runtime;
 
-use crate::input::get_user_input;
+use crate::{input::get_user_input, util::clearscreen_and_show_banner};
 
 mod animeku;
 mod ext;
@@ -14,23 +17,8 @@ mod input;
 mod models;
 mod util;
 
-fn init_app() -> anyhow::Result<()> {
-    clearscreen::clear()?;
-
-    eprintln!(
-        "{} v{} {} val \n",
-        r#"
-  ▄▀█ █▄░█ █ █▀▄▀█ █▀▀ █▄▀ █░█ ▄▄ █▀▀ █░░ █
-  █▀█ █░▀█ █ █░▀░█ ██▄ █░█ █▄█ ░░ █▄▄ █▄▄ █ "#
-            .bright_green(),
-        env!("CARGO_PKG_VERSION"),
-        "©".cyan()
-    );
-    Ok(())
-}
-
 async fn app() -> anyhow::Result<()> {
-    init_app()?;
+    clearscreen_and_show_banner()?;
 
     let input = get_user_input()?;
     let extractor: Box<dyn Ext> = if input.tipe == 0 {
@@ -41,15 +29,15 @@ async fn app() -> anyhow::Result<()> {
 
     let mut animeku = AnimekuCli::new(extractor);
 
-    init_app()?;
+    clearscreen_and_show_banner()?;
     let movie = animeku.search(&input.title).await?;
 
     loop {
-        init_app()?;
+        clearscreen_and_show_banner()?;
         let episode = animeku.extract_episode(movie.clone()).await?;
-        init_app()?;
+        clearscreen_and_show_banner()?;
         let is_anime = episode.is_anime;
-        let download = animeku.extract_download_url(episode).await?;
+        let download = animeku.extract_stream_urls(episode).await?;
 
         print!("{} Membuka tautan diaplikasi eksternal .. ", "◆".blue());
         stdout().flush()?;
